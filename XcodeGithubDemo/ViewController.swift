6ce24863a6d6cacb928c1efec3648357f0d4b59e
sahilbhatia29 <sahilbhatia29@gmail.com>
2020-05-11 16:45:40 +0530

    Changes in view did load

--- XcodeGithubDemo/ViewController.swift
+++ XcodeGithubDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        debugPrint("View did load")
         // Do any additional setup after loading the view.
     }
 
