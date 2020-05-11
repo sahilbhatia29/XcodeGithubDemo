612bd1bd96e03e8f04b20902dd6371791b5852cb
sahilbhatia29 <sahilbhatia29@gmail.com>
2020-05-11 16:48:40 +0530

    Pushing

--- XcodeGithubDemo/ViewController.swift
+++ XcodeGithubDemo/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        debugPrint("View did load")
+        debugPrint("View did load called")
         // Do any additional setup after loading the view.
     }
 
